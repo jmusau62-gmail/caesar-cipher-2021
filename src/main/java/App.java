29dd78b25c4a215e789518d7aa20cf63d2586f8c
jmusau62-gmail <jmusau62@gmail.com>
2021-12-07 20:38:33 +0300

    added comments and instructins to the gui

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,47 +1,46 @@
-
 import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) {
-        Scanner input = new Scanner (System.in);
+        Scanner input = new Scanner(System.in);
+        boolean runningProgram = true;
+        while (runningProgram) {
+            System.out.println("Hey there, this is The Ceasar Cipher, you can choose to encrypt a word, decrypt a word or exit the program. Enter Encrypt, Decrypt or Exit");
+            String directionChoice = input.next();
 
-        boolean programRunning = true;
-        while (programRunning) {
-            System.out.println("Hello, this is The Caesar Cipher, you can choose to encrypt a text, decrypt a text or exit the program. Enter Encrypt, Decrypt or Exit");
-            String navigationChoice = input.next();
+            if (directionChoice.equals("Encrypt")) {
 
-            if (navigationChoice.equals("Encrypt")) {
+                System.out.println("Enter a word to be encrypted: ");
+                String inputText = input.next();
 
-                System.out.println("Enter a text to be encrypted: ");
-                String inputText =input.next();
-
-                System.out.println("Thank you, now enter the shift: ");
+                System.out.println("Great job, now enter the shift: ");
                 Integer inputShift = input.nextInt();
 
                 Caesar inputCaesarEncrypt = new Caesar(inputText, inputShift);
                 String cipherText = inputCaesarEncrypt.encrypt(inputText, inputShift);
 
-                System.out.println("Your encrypted text is " + cipherText);
+                System.out.println("Your encrypted word is " + cipherText);
 
-            } else if (navigationChoice.equals("Decrypt")) {
+            } else if (directionChoice.equals("Decrypt")) {
 
-                System.out.println("Enter an encrypted text to be decrypted: ");
+                System.out.println("Enter an encrypted word to be decrypted: ");
                 String userEncryptedText = input.next();
 
-                System.out.println("Enter the shift of the encrypted text: ");
+                System.out.println("Enter the shift of the encrypted word: ");
                 Integer userEncryptedShift = input.nextInt();
 
                 Caesar inputCaesarDecrypt = new Caesar(userEncryptedText, userEncryptedShift);
                 String decrypted = inputCaesarDecrypt.decrypt(userEncryptedText, userEncryptedShift);
 
-                System.out.println("Your decrypted text is " + decrypted);
-            } else if (navigationChoice.equals("Exit")) {
-                programRunning = false;
+                System.out.println("Your decrypted word is " + decrypted);
+            } else if (directionChoice.equals("Exit")) {
+                runningProgram = false;
             } else {
-                System.out.println("Sorry, your input can't be recognized, choose from Encrypt, Decrypt or Exit please");
+                System.out.println("Oops!, your input is not readable, choose from Encrypt, Decrypt or Exit please");
             }
-
         }
     }
 }
+
+
+
 
