a465843a848470989e5e090e8e8f9be5a063d36a
jmusau62-gmail <jmusau62@gmail.com>
2021-12-07 20:57:07 +0300

    Put Business Logic in the caesar class

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -1,21 +1,21 @@
-public class Caesar {
-    private String plainText;
+public class Caesar  {
+    private String plainWord;
     private int shift;
 
-    public Caesar(String plainText, int shift){
-        this.plainText = plainText;
+    public Caesar(String plainWord, int shift){
+        this.plainWord = plainWord;
         this.shift = shift;
     }
 
-    public String getPlainText(){
-        return plainText;
+    public String getPlainWord(){
+        return plainWord;
     }
 
     public int getShift(){
         return shift;
     }
 
-    public static String encrypt(String plainText, int shift){
+    public static String encrypt(String plainWord, int shift){
         if(shift > 26){
             shift = shift % 26;
         } else if (shift < 0){
@@ -23,9 +23,9 @@ public class Caesar {
         }
 
         String cipherText = "";
-        int length = plainText.length();
+        int length = plainWord.length();
         for(int i = 0; i < length; i++){
-            char character = plainText.charAt(i);
+            char character = plainWord.charAt(i);
             if(Character.isLetter(character)){
                 if(Character.isLowerCase(character)){
                     char ch = (char)(character + shift);
@@ -49,7 +49,7 @@ public class Caesar {
         return cipherText;
     }
 
-    public static String decrypt(String plainText, int shift){
+    public static String decrypt(String plainWord, int shift){
         if(shift > 26){
             shift = shift % 26;
         } else if (shift < 0){
@@ -57,9 +57,9 @@ public class Caesar {
         }
 
         String cipherText = "";
-        int length = plainText.length();
+        int length = plainWord.length();
         for(int i = 0; i < length; i++){
-            char character = plainText.charAt(i);
+            char character = plainWord.charAt(i);
             if(Character.isLetter(character)){
                 if(Character.isLowerCase(character)){
                     char ch = (char)(character - shift);
@@ -84,3 +84,5 @@ public class Caesar {
     }
 
 }
+
+
